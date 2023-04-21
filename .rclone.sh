b5d9c85fc3c88b403cc3934eae5787ea36a004de
Matthew W. Thomas <matthew@mwt.me>
2023-04-21 11:57:18 -0500

    update rclone

--- .rclone.sh
+++ .rclone.sh
@@ -4,16 +4,23 @@
 REMOTE_DIR="dropbox:/Lecture slides"
 SYNC_DIR=`dirname -- $0`
 
+# Change to the directory where the script is located
 cd "$SYNC_DIR"
+
+# Checkout the dropbox branch
+git checkout dropbox && git pull
 
 # tell rclone to return error number 9 when nothing is copied
 rclone sync --error-on-no-transfer "$REMOTE_DIR" "./" --exclude-from ".rcloneignore"
 RET_VAL=$?
 
 if [ $RET_VAL -eq 9 ]; then
-    echo "nothing changed"
+    echo $(date +'[%D %T]') "nothing changed"
 elif [ $RET_VAL -eq 0 ]; then
-    echo "files copied succesfully!"
+    echo $(date +'[%D %T]') "files copied succesfully!"
+    git add -A
+    git commit -c "user.name=Craig Furfine" -c "user.email=c-furfine@kellogg.northwestern.edu" -m "Automatic update from dropbox"
+    git push
 else
-    echo "some other error!"
+    echo $(date +'[%D %T]') "some other error!"
 fi
